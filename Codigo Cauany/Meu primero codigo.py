49458d226bb3a5032578c7c627106b71c1821398
Cauany Depailler <depaillercauany@gmail.com>
2025-08-28 15:49:05 -0300

    subindo códigos

---
+++ Codigo Cauany/Meu primero codigo.py
@@ -0,0 +1,8 @@
+nome = input("Qual é seu nome?")
+print(nome)
+nome = input("Qual e sua idade?")
+print(idade)
+nome = input("Qual sua comida favorita?")
+print(comida) 
+
+
---
+++ Codigo Cauany/Meu segundo progama.py
@@ -0,0 +1,14 @@
+#print("Cauany"*80000)
+#xemplo de variavel, variavel do tipo texto
+fruta ="amora"
+print(type(fruta))
+#exemplo váriavel do tipo INI é interio, número inteiro
+idade =int(35)
+#exemplo de número quebrado, float
+nota = float(9.5)
+
+nome_carro = "porsche gts"
+
+preço = float(1.000.000.00)
+
+ano = int(19498)
